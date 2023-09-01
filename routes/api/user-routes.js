af49c647636c82ddaea9ec40d6fddb8f2e1337fd
Axh <anikahussain1@outlook.com>
2023-09-01 23:42:25 +0100

    routes sent to controllers

---
+++ routes/api/user-routes.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+const userController = require('../../controllers/user-controller');
+
+router
+    .route('/')
+        .get(userController.getUser)
+        .post(userController.createUser);
+
+router
+    .route('/:id')
+        .get(userController.getSingUser)
+        .put(userController.updateUser)
+        .delete(userController.deleteUser);
+router
+    .route('/:userID/friends/:friendID')
+        .post(userController.userFriend)
+        .delete(userController.removeFriend);
+
+module.exports = router;
