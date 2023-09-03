ac84f137614906edcd9b1030bfe9b06b43d23575
Axh <anikahussain1@outlook.com>
2023-09-03 19:35:46 +0100

    spelling corrected

--- models/Thought.js
+++ models/Thought.js
@@ -34,6 +34,6 @@ thoughtSchema
         return this.reactions.length;
     })
 
-const Thought = model('thought', thoughtSchema)
+const Thought = model('Thought', thoughtSchema)
 
 module.exports = Thought;
