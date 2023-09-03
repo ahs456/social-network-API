bc72ac5236a0b7f9bef6987cd04e1b3c363ec8f3
Axh <anikahussain1@outlook.com>
2023-09-03 18:43:04 +0100

    spelling corrected

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes');
-const thoughtRoutes = require('./thoughtt-routes');
+const thoughtRoutes = require('./thought-routes');
 
 router.use('/User', userRoutes);
 router.use('/Thoughts', thoughtRoutes);
